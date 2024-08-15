d2ecfc01e43cd17ddbd470952a962e9ec2ce346a
quang <axellent2004@gmail.com>
2024-08-15 15:45:15 +0700

    ngu si testing

---
+++ control_centre.php
@@ -0,0 +1,29 @@
+<head>
+<style>
+  body{
+        background-color: black !important;
+        padding: 150px;
+        margin: 150px
+    }
+	li a{
+		padding: 10px;
+		font-size: 20px;
+	}
+
+
+
+	</style>
+
+
+<link href="playground.css" rel="stylesheet" >
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+</head>
+<body>
+<h2>Control Centre</h2>
+
+
+<ul class="nav nav-tabs">
+	<li id="feedback"><a href="">Stats</a></li>
+	<li id="users"><a href="user\manage.php">Users</a></li>
+	<li id="Reply"><a href="feedback\manage-fr.php">Reply</a></li> 
+</ul>
